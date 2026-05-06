af808cd40fc4e90b7d2a5103006254121cae4097
RG <loamramg@gmail.com>
2026-05-07 00:26:05 +0530

    chore: eliminate style guide violations

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,5 @@
 exports._check = (x, y) => {
-    if (typeof x !== 'number') {
+  if (typeof x !== 'number') {
     throw new TypeError(`${x} is not a number`);
   }
   if (typeof y !== 'number') {
@@ -13,7 +13,7 @@ exports.add = (x, y) => {
 };
 
 exports.subtract = (x, y) => {
-  exports._check(x, y); 
+  exports._check(x, y);
   return x - y;
 };
 
